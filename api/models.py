173ea4b81cdcb7672a2df3e11a947cf244022831
Njeru Cyrus <njerucyrus5@hotmail.com>
2021-03-03 18:54:50 +0300

    added serializers

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Wallet(models.Model):
+    #same as docKey
+    wallet_id = models.CharField(max_length=128,)
+    user_id = models.CharField(max_length=128,)
+    current_balance = models.DecimalField(max_digits=9, decimal_places=2)
+    last_updated = models.DateTimeField(auto_now_add=True)
