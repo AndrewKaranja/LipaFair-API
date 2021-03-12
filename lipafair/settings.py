02d58774847502d16feaaf0ec014ccde89bbffe1
Njeru Cyrus <njerucyrus5@hotmail.com>
2021-03-12 03:18:34 +0300

    updated static files

--- lipafair/settings.py
+++ lipafair/settings.py
@@ -128,17 +128,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static_files'),
+    os.path.join(BASE_DIR, 'static'),
 )
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_root')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, '/media_files/')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
